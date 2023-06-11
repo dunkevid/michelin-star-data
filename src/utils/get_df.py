133e9b293e70b1352663190141e48d111dfaab4f
0x9e2fa22b <huyc26.dev@gmail.com>
2023-06-11 13:46:50 +0700

    feat: transform restaurants data

--- src/utils/get_df.py
+++ src/utils/get_df.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from src.utils.get_file_path import \
   get_latest_michelin_data_file_path, \
@@ -6,7 +7,27 @@ from src.utils.get_file_path import \
 
 def get_latest_restaurants_df():
   path = get_latest_michelin_data_file_path()
-  df = pd.read_json(path)
+
+  with open(path) as file:
+    json_data = json.load(file)
+
+  df = pd.json_normalize(json_data)
+
+  df.rename(
+    columns={
+      'price.raw': 'price_raw',
+      'price.symbol': 'price_currency',
+      'price.rate': 'price_rate',
+      'awards.star': 'star',
+      'awards.is_green_star': 'is_green_star',
+      'awards.is_bib_gourmand': 'is_bib_gourmand',
+      'position.latitude': 'latitude',
+      'position.longtitude': 'longtitude',
+      'position.location.region': 'region',
+      'position.location.city': 'city',
+    },
+    inplace=True
+  )
 
   return df
 
